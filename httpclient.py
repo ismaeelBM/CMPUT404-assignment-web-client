b7f6af5e4dd8fcb07bcffdecf1cf6346b47ece7f
ismaeelBM <ismaeel@ualberta.ca>
2021-10-03 02:25:07 -0400

    all test passed

--- httpclient.py
+++ httpclient.py
@@ -43,10 +43,12 @@ class HTTPClient(object):
         
         if "/" in host:
             (host, path) = host.split("/", 1)
+            path = "/" + path
 
         if ":" in host:
             try:
                 port = int(host.split(":")[-1])
+                host = host.split(":", 1)[0]
             except:
                 port = 80
 
@@ -55,9 +57,12 @@ class HTTPClient(object):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        print("Connected to", host, port)
-        return None
+        try:
+            self.socket.connect((host, port))
+        except:
+            self.socket.close()
+            return False
+        return True
 
     def get_code(self, data):
         try:
@@ -92,29 +97,63 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
+        code = 404
         body = ""
         (host, port, path) = self.get_host_port_path(url)
         payload = f'GET {path} HTTP/1.0\r\nHost: {host}\r\n\r\n'
+        if (self.connect(host, port)):
+            self.sendall(payload)
+            self.socket.shutdown(socket.SHUT_WR)
 
-        self.connect(host, port)
-        self.sendall(payload)
-        self.socket.shutdown(socket.SHUT_WR)
+            data = self.recvall(self.socket)
 
-        data = self.recvall(self.socket)
+            header = self.get_headers(data)
+            body = self.get_body(data)
+            code = self.get_code(header)
 
-        header = self.get_headers(data)
-        body = self.get_headers(data)
-        code = self.get_code(header)
+            print(data)
 
-        print(data)
+            self.close()
 
-        return HTTPResponse(code, body)
+            return HTTPResponse(code, body)
+        else:
+            return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
+        code = 404
         body = ""
-        return HTTPResponse(code, body)
+        post_body = ""
+        (host, port, path) = self.get_host_port_path(url)
+        payload = f'POST {path} HTTP/1.0\r\nHost: {host}\r\n'
+        if args != None:
+            for key in args.keys():
+                post_body += str(key) + "=" + str(args[key]) + "&"
+            post_body = post_body[:-1]
+            length = len(post_body)
+            payload += f"Content-Length: {length}\r\nContent-Type:application/x-www-form-urlencoded \r\n"
+
+        else:
+            payload += f"Content-Length: 0\r\nContent-Type:application/x-www-form-urlencoded \r\n"
+
+        payload += "\r\n"
+        payload += post_body
+        if (self.connect(host, port)):
+            self.sendall(payload)
+            self.socket.shutdown(socket.SHUT_WR)
+
+            data = self.recvall(self.socket)
+
+            header = self.get_headers(data)
+            body = self.get_body(data)
+            code = self.get_code(header)
+
+            print(data)
+
+            self.close()
+
+            return HTTPResponse(code, body)
+        else:
+            return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
